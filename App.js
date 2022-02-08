ebc0bbb8506e19c110594b83dd3c2a38dd538e06
JoySystem_v <fvv2011@gmail.com>
2022-02-08 11:14:14 +0300

    change version for publish

--- App.js
+++ App.js
@@ -1,25 +1,148 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useRef, useState} from 'react';
 
-import { Button, Platform, SafeAreaView, StyleSheet, Text, View, Pressable, useRef, useState} from 'react-native';
+import {ActivityIndicator, Button, Platform, SafeAreaView, StyleSheet, Text, View, Pressable,  BackHandler} from 'react-native';
 import { WebView } from 'react-native-webview';
 import Icon from 'react-native-ico-material-design';
 
+const iconWidth = 24;
+const iconHeight = 24;
+
+function LoadingIndicatorView() {
+
+  console.log("Indicator changed");
+  return <ActivityIndicator color='#009b88' size='large' />
+}
+
+
+
 const App = () => {
   const webview = useRef();
+  const [hasInjectedVariable, setHasInjectedVariable] = useState(false);
+  const [targetUrl, setTargetUrl] = useState('https://restrun.ru/');
+  const [keyState, setKeyState] = useState(1);
+
+  const handleNavigationStateChanged = (event) => {
+    if(!event.loading && !hasInjectedVariable){
+        webview.current.injectJavaScript('window.isNative = true');
+        setHasInjectedVariable(true);
+    }
+}
+
+
 
 
   return (
     
     
-    
-    <WebView
+    <SafeAreaView style = {styles.container} >
+       <StatusBar backgroundColor = "#ffffff"  />
+        <WebView
+            style = {{flex:0.92, marginTop: 25}}
             ref={webview}
             onNavigationStateChange={handleNavigationStateChanged}
-            source={{ uri: 'https://restrun.ru/' }}
+            source={{ uri: targetUrl }}
+            allowsBackForwardNavigationGestures
+            originWhitelist = {
+              ["https://*", "http://*", "file://*", "sms://*"]
+            }
+            renderLoading={LoadingIndicatorView}
         />
 
+<View style = {[styles.NavContainer, styles.elevation]} >
 
+      <View style = {[styles.NavBar]}>
+
+      <Pressable onPress = {
+        () => {
+          if (targetUrl == 'https://restrun.ru/'){
+          setTargetUrl('https://restrun.ru/#popup-address');
+        }else{
+          setTargetUrl('https://restrun.ru/');
+        }
+          setKeyState(keyState+1);
+        }
+      }
+      style = {
+        [styles.IconBehave]
+      }
+      android_ripple = {
+        {
+          borderless: true,
+          radius: 50
+        }
+      } >
+
+
+      <Icon name = "home-button"
+      height = {
+        iconHeight
+      }
+      width = {
+        iconWidth
+      }
+      color = "#ff6347" / >
+
+      </Pressable>
+
+      <Pressable onPress = {
+        () => {
+          webview.current.injectJavaScript('document.querySelector(".header__login a.header__btn").click(); true;');
+        }
+      }
+      style = {
+        [styles.IconBehave]
+      }
+      android_ripple = {
+        {
+          borderless: true,
+          radius: 50
+        }
+      } >
+
+      <Icon name = "map-placeholder"
+      height = {
+        iconHeight
+      }
+      width = {
+        iconWidth
+      }
+      color = "#ff6347" / >
+
+
+      </Pressable>
+
+      <Pressable onPress = {
+        () => {
+          webview.current.injectJavaScript('document.querySelector(".header__login a.header__login-enter").click(); true;');
+        }
+      }
+      style = {
+        [styles.IconBehave]
+      }
+      android_ripple = {
+        {
+          borderless: true,
+          radius: 50
+        }
+      } >
+
+      <Icon name = "user-shape"
+      height = {
+        iconHeight
+      }
+      width = {
+        iconWidth
+      }
+      color = "#ff6347" / >
+
+      </Pressable> 
+      </View> 
+      </View>
+
+
+
+     </SafeAreaView>
 
   );
 }
@@ -27,10 +150,42 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
+  NavContainer: {
+    flex: 0.08,
+    backgroundColor: '#f8f9fa',
+    borderTopWidth: 1,
+    borderTopColor: "rgba(52,58,64,.1)",
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 0,
+
+
+  },
+
+
+  elevation: {
+    elevation: 20,
+    shadowColor: '#000',
+
+  },
+  NavBar: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    backgroundColor: '#f8f9fa'
+  },
+
+
+  IconBehave: {
+
+    padding: 20,
+
+
+  }
+
 });
 
 
